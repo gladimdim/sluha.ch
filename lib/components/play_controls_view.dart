4ec0b0a5031dcdfe156eda2324ddf7015e86ed49
Dmytro Gladkyi <gladimdim@gmail.com>
2021-10-26 22:45:58 +0300

    Improved landscape mode controls

--- lib/components/play_controls_view.dart
+++ lib/components/play_controls_view.dart
@@ -4,7 +4,6 @@ import 'package:audiobooks_app/components/icon_button_styled.dart';
 import 'package:audiobooks_app/components/interactive_slider.dart';
 import 'package:audiobooks_app/models/book.dart';
 import 'package:audiobooks_app/models/player.dart';
-import 'package:audiobooks_app/utils.dart';
 import 'package:flutter/material.dart';
 
 final playerControlsKey = GlobalKey();
@@ -16,104 +15,89 @@ class PlayControlsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Player player = Player.instance;
-    final portrait = isPortrait(MediaQuery.of(context).size);
     return Material(
       color: Theme.of(context).primaryColor,
       child: Container(
         padding: EdgeInsets.only(top: 2.0),
         // color: Theme.of(context).primaryColor,
-        child: Column(
+        child: Row(
           children: [
-            if (!portrait)
-              Expanded(
-                flex: 2,
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
                 child: StreamBuilder(
                   stream: player.playbackChanges,
                   builder: (context, snapshot) {
-                    return BookCover(book: Player.instance.book);
+                    return GestureDetector(
+                      onTap: () {
+                        if (onNavigateToBookPress != null) {
+                          onNavigateToBookPress!(Player.instance.book);
+                        }
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        child: BookCover(book: Player.instance.book),
+                      ),
+                    );
                   },
                 ),
               ),
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
+            ),
+            Expanded(
+              flex: 8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 2.0, left: 8.0, right: 8.0),
                     child: StreamBuilder(
                       stream: player.playbackChanges,
-                      builder: (context, snapshot) {
-                        return GestureDetector(
-                          onTap: () {
-                            if (onNavigateToBookPress != null) {
-                              onNavigateToBookPress!(Player.instance.book);
-                            }
-                          },
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                            child: BookCover(book: Player.instance.book),
-                          ),
-                        );
-                      },
+                      builder: (context, data) => CurrentlyPlaying(
+                        Player.instance.currentFile,
+                        player.getBookTitle,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 8,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  InteractiveSlider(
+                    player: player,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 2.0, left: 8.0, right: 8.0),
-                        child: StreamBuilder(
+                      IconButtonStyled(
+                          iconData: Icons.skip_previous_outlined,
+                          onPressed: player.playPrevious),
+                      StreamBuilder<bool>(
                           stream: player.playbackChanges,
-                          builder: (context, data) => CurrentlyPlaying(
-                            Player.instance.currentFile,
-                            player.getBookTitle,
-                          ),
-                        ),
-                      ),
-                      InteractiveSlider(
-                        player: player,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          IconButtonStyled(
-                              iconData: Icons.skip_previous_outlined,
-                              onPressed: player.playPrevious),
-                          StreamBuilder<bool>(
-                              stream: player.playbackChanges,
-                              builder: (context, data) {
-                                if (data.hasData) {
-                                  var isPlaying = data.data!;
-                                  if (isPlaying) {
-                                    return IconButtonStyled(
-                                        iconData:
-                                            Icons.pause_circle_filled_outlined,
-                                        onPressed: player.pause);
-                                  } else {
-                                    return IconButtonStyled(
-                                        iconData:
-                                            Icons.play_circle_filled_outlined,
-                                        onPressed: () => player.resume());
-                                  }
-                                } else {
-                                  return IconButtonStyled(
-                                    iconData: Icons.play_circle_filled_outlined,
-                                  );
-                                }
-                              }),
-                          IconButtonStyled(
-                              iconData: Icons.skip_next_outlined,
-                              onPressed: player.playNext),
-                        ],
-                      ),
+                          builder: (context, data) {
+                            if (data.hasData) {
+                              var isPlaying = data.data!;
+                              if (isPlaying) {
+                                return IconButtonStyled(
+                                    iconData:
+                                        Icons.pause_circle_filled_outlined,
+                                    onPressed: player.pause);
+                              } else {
+                                return IconButtonStyled(
+                                    iconData:
+                                        Icons.play_circle_filled_outlined,
+                                    onPressed: () => player.resume());
+                              }
+                            } else {
+                              return IconButtonStyled(
+                                iconData: Icons.play_circle_filled_outlined,
+                              );
+                            }
+                          }),
+                      IconButtonStyled(
+                          iconData: Icons.skip_next_outlined,
+                          onPressed: player.playNext),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
